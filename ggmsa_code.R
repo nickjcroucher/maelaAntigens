3563380318fbbf4062b2cd64989b04e11d0559e4
Nick Croucher <n.croucher@imperial.ac.uk>
2023-10-22 21:22:40 +0100

    Add bar functions

--- ggmsa_code.R
+++ ggmsa_code.R
@@ -26,6 +26,51 @@ c("A" = "orange",
 "-" = "grey",
 "*" = "black")
 
+geom_msaBar <- function() {
+  structure(list(),
+            class = "msaBar")
+}
+
+ly_bar <- function(tidy){
+  data <- bar_data(tidy)
+  mapping <- aes_(x = ~pos, y = ~Freq, fill = ~Freq)
+  ly_bar <- geom_col(data = data, mapping = mapping, width = 1, show.legend = F)
+  return(ly_bar)
+}
+
+bar_data <- function(tidy){
+  character_position <- unique(tidy$position)
+  conservation_score <- lapply(character_position, function(j) {
+    cloumn_data <- tidy[tidy$position == j, ]
+    character_frequency <- table(cloumn_data$character) %>% as.data.frame
+    #character_frequency <- data.frame(character_frequency)
+    max_frequency <- character_frequency[character_frequency[2] == max(character_frequency[2]),]
+    max_frequency$Var1 <- as.character(max_frequency$Var1)
+    if(nrow(max_frequency) == 1) {
+      max_frequency <- max_frequency[1,]
+    }else {
+      max_frequency <- max_frequency[1,]
+    }
+  }) %>% do.call("rbind", .)
+  conservation_score["pos"] <- character_position
+  return(conservation_score)
+}
+
+theme_msa <- function(){
+  list(
+    xlab(NULL),
+    ylab(NULL),
+    labs(fill = "Fills"),
+    coord_fixed(),
+    scale_x_continuous(expand = c(0,0)),
+    theme_minimal() +
+      theme(
+        strip.text = element_blank(),
+        panel.spacing.y = unit(.4, "in"),
+        panel.grid = element_blank())
+  )
+}
+
 color_scheme <- function(y, color = "Chemistry_AA", custom_color = NULL) {
     y$color <- unlist(lapply(y$character, function (x) {as.character(aa_colours[names(aa_colours)==x])}))
     return(y)
